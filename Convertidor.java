062d28d3c0c41fa500d96891ed7bfa5daf504198
einarfr0g <einarzarate@ciencias.unam.mx>
2024-02-11 22:52:01 -0600

    commit de con metodos (convertir10a1) y (convertir16a2)

--- Convertidor.java
+++ Convertidor.java
@@ -5,6 +5,16 @@ public class Convertidor {
     public String numeroAConvertir;
 
     public int base;
+    
+    // -------Constructor------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    
+    public Convertidor(){
+        numeroAConvertir="";
+
+
+        base = -1; 
+    }
+    
 
     // -------SETTERS Y GETTERS------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -46,5 +56,152 @@ public class Convertidor {
         return base;
     }
 
-    
+    // -------METODOS DE A DEBIS------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * conversión de número en String de base 10 a base 2, puede soltar excepción pues usa Integer.valueOf(num) si la String contiene algúna caracter no numérico
+     * @param num
+     * @return String del número convertido a binario
+     */
+    public String conversion10a2(String num){
+        int n;
+        String resultado="";
+
+        n = Integer.valueOf(num);
+
+        System.out.println(n);
+
+        while((n!=0)){
+            resultado = (n%2)+resultado;
+            n=n/2;
+        }
+
+        return resultado;
+    }
+
+    /**
+     * hola pao, ya quedó solo falta soltar la excepción en otro caso, ponlo porfa es que no supe como ponerlo
+     * @param num
+     * @return String del número en base 2
+     * @throws IllegalArgumentException
+     */
+    public String conversion8a2(String num) throws IllegalArgumentException{
+        String resultado = "";
+
+        for(int i=0; i<num.length();i++){
+            switch (num.charAt(i)) {
+                case '0':
+                       resultado = resultado + "000"; 
+                    
+                    break;
+                case '1':
+                        resultado = resultado + "001";
+
+                    break;
+                case '2':
+                        resultado = resultado + "010";
+
+                    break;
+                case '3':
+                        resultado = resultado +"011"; 
+                    break;
+                case '4':
+                        resultado = resultado +"100"; 
+                    break;
+                case '5':
+                        resultado = resultado +"101"; 
+                    break;
+                case '6':
+                        resultado = resultado +"110"; 
+                    break;
+                case '7':
+                        resultado = resultado +"111"; 
+                    break;
+
+                default://we acá no se como ponerle que en otro caso mande invalid argument exception porfa ponlo tú 
+                        //throw new IllegalArgumentException("La String del número contiene un dígito que no está en base 8");
+                
+            }
+
+        }
+
+        return resultado;
+    }
+
+    /**
+     * hola pao, ya quedó solo falta soltar la excepción en otro caso, ponlo porfa es que no supe como ponerlo
+     * @param num
+     * @return String del número en base 2
+     * @throws IllegalArgumentException
+     */
+    public String conversion16a2(String num) throws IllegalArgumentException{
+        String resultado = "";
+
+        for(int i=0; i<num.length();i++){
+            switch (num.charAt(i)) {
+                case '0':
+                       resultado = resultado + "0000"; 
+                    
+                    break;
+                case '1':
+                        resultado = resultado + "0001";
+
+                    break;
+                case '2':
+                        resultado = resultado + "0010";
+
+                    break;
+                case '3':
+                        resultado = resultado +"0011"; 
+                    break;
+                case '4':
+                        resultado = resultado +"0100"; 
+                    break;
+                case '5':
+                        resultado = resultado +"0101"; 
+                    break;
+                case '6':
+                        resultado = resultado +"0110"; 
+                    break;
+                case '7':
+                        resultado = resultado +"0111"; 
+                    break;
+
+                case '8':
+                       resultado = resultado + "1000"; 
+                    
+                    break;
+                case '9':
+                        resultado = resultado + "1001";
+
+                    break;
+                case 'A':
+                        resultado = resultado + "1010";
+
+                    break;
+                case 'B':
+                        resultado = resultado +"1011"; 
+                    break;
+                case 'C':
+                        resultado = resultado +"1100"; 
+                    break;
+                case 'D':
+                        resultado = resultado +"1101"; 
+                    break;
+                case 'E':
+                        resultado = resultado +"1110"; 
+                    break;
+                case 'F':
+                        resultado = resultado +"1111"; 
+                    break;
+
+                default://we acá no se como ponerle que en otro caso mande invalid argument exception porfa ponlo tú 
+                        //throw new IllegalArgumentException("La String del número contiene un dígito que no está en base 16");
+                
+            }
+
+        }
+
+        return resultado;
+    }
 }
---
+++ Pruebas.java
@@ -0,0 +1,9 @@
+public class Pruebas {
+    public static void main(String[] args) {
+        Convertidor nuevo = new Convertidor();
+
+        String binario  = nuevo.conversion16a2("265F");
+
+        System.out.println(binario);
+    }
+}
